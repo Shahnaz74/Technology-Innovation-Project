d46e38cdf4d1d09ea43384de98f67fd83ee0d0e9
derek030 <derekchans@yahoo.com.hk>
2023-04-25 21:49:37 +1000

    update home page with BS

--- Frontend/js/custom-js.js
+++ Frontend/js/custom-js.js
@@ -7,3 +7,16 @@ $(document).ready(function () {
 
 // Toast message
 $(".toast").toast("show");
+
+// home search bar dropdown list
+$(document).ready(function () {
+  $(".default_option").click(function () {
+    $(".dropdown ul").addClass("active");
+  });
+
+  $(".dropdown ul li").click(function () {
+    var text = $(this).text();
+    $(".default_option").text(text);
+    $(".dropdown ul").removeClass("active");
+  });
+});
