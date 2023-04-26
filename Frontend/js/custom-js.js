85a974260c0a88be13035ea963121c13262608c3
berylleung <beryl.hyleung@gmail.com>
2023-04-26 10:34:37 +1000

    Added admin portal submit record and edit record page

--- Frontend/js/custom-js.js
+++ Frontend/js/custom-js.js
@@ -7,3 +7,16 @@ $(document).ready(function () {
 
 // Toast message
 $(".toast").toast("show");
+
+// Sticky header
+const header = document.getElementById("form-header");
+const intercept = document.createElement("div");
+
+intercept.setAttribute("data-observer-intercept", "");
+header.before(intercept);
+
+const observer = new IntersectionObserver(([entry]) => {
+  header.classList.toggle("active", !entry.isIntersecting);
+});
+
+observer.observe(intercept);
